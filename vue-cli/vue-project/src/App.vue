d40f2260e59a8dc315c271097c6643c09eb66d8e
Zeromnimusse <va@opendg.ru>
2020-03-17 13:16:03 +0700

    Текстовый инпут VUE JS
    Текстовый инпут VUE JS

--- vue-cli/vue-project/src/App.vue
+++ vue-cli/vue-project/src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <div>
     <h2>{{title}}</h2>
-    <input type="text" v-model="searchName">
-    <ul>
-      <li v-for="name in filteredNames"> {{ name }}</li>
-    </ul>
-    <hr>
-    <app-list></app-list>
+    <input type="text" v-model.lazy="name">
+    <p>{{name}}</p>
   </div>
 
 </template>
 
 <script>
-  import ListMixin from './listMixin'
-export default {
+  export default {
     data() {
       return {
-        title: 'Использование миксинов VUE JS',
+        title: 'Текстовый инпут VUE JS',
+        name: 'Initial state'
       }
     },
-  mixins: [ListMixin],
-    filters: {
-      lowercase(value) {
-        return value.toLowerCase()
-      }
-    }
   }
 
 </script>
-
-<style scoped>
-
-</style>
--- vue-cli/vue-project/src/main.js
+++ vue-cli/vue-project/src/main.js
@@ -1,21 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import List from './List.vue'
-
-
-
-Vue.filter('uppercase', (value) => {
-  return value.toUpperCase()
-})
-
-Vue.component('app-list', List)
-
-Vue.mixin({
-  beforeCreate() {
-    console.log('beforeCreated')
-  }
-})
-
 new Vue({
   el: '#app',
   render: h => h(App)
