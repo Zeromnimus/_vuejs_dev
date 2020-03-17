38dbed5d2bf43e6ed256d37343c679fe867c89ce
Zeromnimusse <va@opendg.ru>
2020-03-17 16:48:44 +0700

    Разработка ползунка
    Начало разработки ползунка

--- vue-cli/vue-components/src/App.vue
+++ vue-cli/vue-components/src/App.vue
@@ -2,39 +2,17 @@
 
   <div class="container">
     <h1>{{title}}</h1>
-    <form class="pt-3">
-    <div class="form-group">
-      <label for="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        class="form-control"
-        :class="{'is-invalid': $v.email.$error}"
-        @blur="$v.email.$touch()"
-        v-model="email"
-      >
-      <div class="invalid-feedback" v-if="!$v.email.required">Email field is required.</div>
-      <div class="invalid-feedback" v-if="!$v.email.email">This field should be an email.</div>
-    </div>
-    </form>
+
   </div>
 </template>
 
 <script>
-  import { required, email } from 'vuelidate/lib/validators'
 export default {
   data () {
       return {
         email: '',
-        title: "Визуальное отображение ошибок VUE JS"
+        title: "Тестирование компонента ползунок!"
       }
     },
-      validations: {
-        email: {
-          required: required,
-          email:email
-        }
-    }
   }
-
 </script>
