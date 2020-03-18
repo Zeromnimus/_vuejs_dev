851468bb4e524f47dc35f6ffeeae1b04bc048aa5
Zeromnimusse <va@opendg.ru>
2020-03-18 11:15:03 +0700

    Черновик ползунка
    Черновик ползунка с картинками

--- vue-cli/vue-components/src/App.vue
+++ vue-cli/vue-components/src/App.vue
@@ -1,18 +1,31 @@
 <template>
-
   <div class="container">
     <h1>{{title}}</h1>
-
+    <ThumbSlider
+    ></ThumbSlider>
+    <hr>
+    <input type="text" v-model="parentValue" @keypress.enter="setValue">
   </div>
 </template>
 
 <script>
+import ThumbSlider from "./ThumbSlider";
+import {EventEmitter} from './main.js'
 export default {
-  data () {
+    data () {
       return {
-        email: '',
-        title: "Тестирование компонента ползунок!"
+        title: "Тестирование компонента ползунок!",
+        parentValue: 0
       }
+    },
+    methods: {
+      setValue() {
+        EventEmitter.$emit('onSetValue', this.parentValue )
+        console.log(this.parentValue)
+      }
+    },
+    components: {
+      ThumbSlider: ThumbSlider
     },
   }
 </script>
---
+++ vue-cli/vue-components/src/ThumbSlider.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="slidecontainer">
+      <input type="range"
+             :min="minVal"
+             :max="maxVal"
+             class="slider"
+             id="myRange"
+             v-model="value"
+      >
+      <p>Value: <span id="demo">{{value}}</span></p>
+    </div>
+</template>
+
+<script>
+  import {EventEmitter} from "./main";
+
+  export default {
+      name: "ThumbSlider",
+      data () {
+        return {
+          minVal: 1,
+          maxVal: 100,
+          value: 50,
+        }
+      },
+      created() {
+        EventEmitter.$on('onSetValue', (nval) => {
+          this.value = nval
+        })
+      }
+    }
+</script>
+
+<style scoped>
+  .slidecontainer {
+    width: 100%;
+  }
+
+  .slider {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 10px;
+    border-radius: 5px;
+
+    background: url('./assets/line.png');
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+    border: 1px solid black;
+  }
+
+  .slider:hover {
+    opacity: 1;
+  }
+
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 23px;
+    height: 24px;
+    border: 0;
+    background: url('./assets/pipka.png');
+    cursor: pointer;
+  }
+
+  .slider::-moz-range-thumb {
+    width: 23px;
+    height: 24px;
+    border: 0;
+    background: url('./assets/pipka.png');
+    cursor: pointer;
+  }
+</style>
